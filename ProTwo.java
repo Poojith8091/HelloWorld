46df20985bb29ecf27b59f817678518e07f1616f
poojith8091 <poojith.g.v.s@gmail.com>
2019-12-04 11:32:19 +0530

    Deleted Pip

--- ProTwo.java
+++ ProTwo.java
@@ -1,9 +1,9 @@
-public class Pip {
+public class ProTwo {
     public static void main(String[] args) {
         int rows = 5;
         for(int i = 1; i <= rows; ++i) {
             for(int j = 1; j <= i; ++j) {
-                System.out.print("* ");
+                System.out.print(j + " ");
             }
             System.out.println();
         }
