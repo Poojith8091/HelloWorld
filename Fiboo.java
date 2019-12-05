ca810b309941397f6e6eb5ae542a979aef4ac5f1
poojith8091 <poojith.g.v.s@gmail.com>
2019-12-05 09:15:39 +0530

    Fiboo

--- Fiboo.java
+++ Fiboo.java
@@ -1,6 +1,6 @@
 public class Fiboo {
     public static void main(String[] args) {
-        int n = 10, t1 = 0, t2 = 1;
+        int n = 11, t1 = 0, t2 = 1;
         System.out.print("First " + n + " terms: ");
         for (int i = 1; i <= n; ++i)
         {
