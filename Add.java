9b56e761525cdf3ed07d2b563b284d5b89d81013
poojith8091 <poojith.g.v.s@gmail.com>
2019-12-05 09:02:31 +0530

    addition

--- Add.java
+++ Add.java
@@ -1,8 +1,8 @@
-public class AddTwoNumbers {
+public class Add {
 
    public static void main(String[] args) {
         
-      int num1 = 10, num2 = 15, sum;
+      int num1 = 20, num2 = 15, sum;
       sum = num1 + num2;
 
       System.out.println("Sum of these numbers: "+sum);
